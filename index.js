6c1b9bae3bac91b6e08baa7117cd6d2d97b25104
am-ikharo <85495558+am-ikharo@users.noreply.github.com>
2025-05-28 02:05:02 +0100

    added 2 html files

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
 import http from 'http';
-const PORT = process.env.PORT
+import fs from 'fs/promises';
+import url from 'url';
+import path from 'path'
+import { error } from 'console';
+const PORT = process.env.PORT;
 
-const Server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/html'});
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const Server = http.createServer( async (req, res) => {
+    try {
+        if(req.method === 'GET'){
+            let filepath;
+            if (req.url === '/'){
+                filepath= path.join(__dirname, 'public', 'index.html');
+                
     
-    res.end('<h1>Hello World</h>')
+            }else if (req.url === '/about'){
+                filepath= path.join(__dirname, 'public', 'about.html')
+                
+            }else{
+                throw new Error ("Page Not Found")
+            }
+
+            const data = await fs.readFile(filepath);
+            res.setHeader('Content-Type', 'text/html');
+            res.write(data);
+            res.end();
+        }else{
+            throw new Error ('Method Nokt Allowed')
+        }
+    } catch (error) {
+        res.writeHead(500, {'Content-Type': 'text/html'})
+        res.end('Server error')
+        
+    }
+    
 
 });
 
